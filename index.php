ced918be212eacc5b763092df4b1b21d961f802e
Matt MacNeil <matt@mattmacneil.com>
2017-06-13 15:10:04 -0400

    Add and style who we are section (fullscreen).

--- index.php
+++ index.php
@@ -26,6 +26,16 @@
 
 								<div class="hero-imagebox-overlay">
 
+									<div class="hero-imagebox-quote">
+
+										<span>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.&rdquo;</span>
+
+										<br />
+
+										<span>- Cpl Daniel Smith, CodeAcademy Graduate</span>
+
+									</div><!-- hero imagebox quote -->
+
 								</div><!-- hero imagebox overlay -->
 
 							</div><!-- hero imagebox overlay inner -->
@@ -47,7 +57,56 @@
 
 		<div class="content-container">
 
-			hi
+			<span class="headliner">What We Do:</span>
+
+			<div class="what-we-do-container">
+
+				<div class="what-we-do-cell">
+
+					<div class="what-we-do-image what-we-do-image-1">
+					</div><!-- what we do image -->
+
+					<div class="what-we-do-content">
+
+						<h2>Career Services:</h2>
+
+						Our career services team provides job opportunities, career guidance and resume reviews, and prep for technical interviews.
+
+					</div><!-- what we do content -->
+
+				</div><!-- what we do cell -->
+
+				<div class="what-we-do-cell">
+
+					<div class="what-we-do-image what-we-do-image-2">
+					</div><!-- what we do image -->
+
+					<div class="what-we-do-content">
+
+						<h2>Mentor Program:</h2>
+
+						Our career services team provides job opportunities, career guidance and resume reviews, and prep for technical interviews.
+
+					</div><!-- what we do content -->
+
+				</div><!-- what we do cell -->
+
+				<div class="what-we-do-cell">
+
+					<div class="what-we-do-image what-we-do-image-3">
+					</div><!-- what we do image -->
+
+					<div class="what-we-do-content">
+
+						<h2>Conference Scholarships:</h2>
+
+						Our career services team provides job opportunities, career guidance and resume reviews, and prep for technical interviews.
+
+					</div><!-- what we do content -->
+
+				</div><!-- what we do cell -->
+
+			</div><!-- what we do container -->
 
 		</div><!-- content container -->
 		
