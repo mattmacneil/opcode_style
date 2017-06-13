87075b8bbe618e9739cd829df8bda8157dcd23fd
Matt MacNeil <matt@mattmacneil.com>
2017-06-13 12:50:48 -0400

    Added and styled nav content, added hero and hero box.

--- index.php
+++ index.php
@@ -1,13 +1,42 @@
 <?php
     include "head.php";
-   // include "nav.php";
 ?>
 
 	<div class="hero">
 
 		<div class="hero-overlay">
 
-			hi
+			<?php include "nav.php"; ?>
+
+			<div class="content-container">
+
+				<div class="hero-content">
+
+					<h1>
+						The largest community dedicated to helping military veterans and families launch software development careers.
+					</h1>
+
+				</div><!-- hero content -->
+
+				<div class="hero-imagebox">
+
+					<div class="hero-imagebox-overflow">
+
+							<div class="hero-imagebox-overflow-inner">
+
+								<div class="hero-imagebox-overlay">
+
+									hi
+
+								</div><!-- hero imagebox overlay -->
+
+							</div><!-- hero imagebox overlay inner -->
+
+					</div><!-- hero imagebox overlay -->
+
+				</div><!-- hero imagebox -->
+
+			</div><!-- content container -->
 
 		</div><!-- hero overlay -->
 
---
+++ nav.php
@@ -0,0 +1,33 @@
+			<nav>
+				
+				<div class="content-container">
+
+					<div class="nav-logo">
+
+						<a href="index.php">
+							<img src="images/nav-logo.png" alt="Operation Code Home" />
+						</a>
+
+					</div><!-- nav logo -->
+
+					<div class="nav-links">
+
+						<div class="nav-links-align">
+
+							<a href="XXX">Code Schools</a>
+
+							<a href="XXX">Donate</a>
+
+							<a href="XXX">Log In</a>
+
+							<a href="XXX">Join Now!</a>
+
+						</div><!-- nav links align -->
+
+					</div><!-- nav links -->
+
+					<div class="clearfix"></div>
+
+				</div><!-- nav content container -->
+
+			</nav>
